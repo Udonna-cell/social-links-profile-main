1031fb6dfe596443a78dda923d5033c9c97ae3d0
udonna-cell <orabuezestanley@gmail.com>
2024-01-20 03:12:32 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import profile from "./assets/images/avatar-jessica.jpeg";
+import "./App.css";
 
+function Buttton({name}) {
+  return (
+    <button>{name}</button>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="card">
+      <figure>
+        <img src={profile} alt="profile" draggable="false" />
+      </figure>
+      <h1>Jessica Randall</h1>
+      <p className="location">London, United Kingdom</p>
+      <p>"Front-end developer and avid reader."</p>
+      <Buttton name="GitHub" />
+      <Buttton name="Frontend Mentor" />
+      <Buttton name="LinkedIn" />
+      <Buttton name="Twitter" />
+      <Buttton name="Instagram" />
+    </section>
   );
 }
 
